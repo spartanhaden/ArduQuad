c060629d7010b75ea669e0ac905a75bc37766667
Haden Wasserbaech <spartanhaden@gmail.com>
2014-11-26 23:36:06 -0500

    Read sensor method
    Added a read sensor method

--- Accelerometer.ino
+++ Accelerometer.ino
@@ -11,17 +11,21 @@ void AccelLoop(){
 }
 
 void printAccelVals(){
-	Serial.print("ACEL X: ");
-	Serial.print(readByte(MPU_ADDR, ACCEL_XOUT_L)|(readByte(MPU_ADDR, ACCEL_XOUT_H) << 8));
-	Serial.print("\tACEL Y: ");
-	Serial.print(readByte(MPU_ADDR, ACCEL_YOUT_L)|(readByte(MPU_ADDR, ACCEL_YOUT_H) << 8));
-	Serial.print("\tACEL Z: ");
-	Serial.println(readByte(MPU_ADDR, ACCEL_ZOUT_L)|(readByte(MPU_ADDR, ACCEL_ZOUT_H) << 8));
-	Serial.print("GYRO X: ");
-	Serial.print(readByte(MPU_ADDR, GYRO_XOUT_L)|(readByte(MPU_ADDR, GYRO_XOUT_H) << 8));
-	Serial.print("\tGYRO Y: ");
-	Serial.print(readByte(MPU_ADDR, GYRO_YOUT_L)|(readByte(MPU_ADDR, GYRO_YOUT_H) << 8));
-	Serial.print("\tGYRO Z: ");
-	Serial.println(readByte(MPU_ADDR, GYRO_ZOUT_L)|(readByte(MPU_ADDR, GYRO_ZOUT_H) << 8));
+	Serial.print("A X: ");
+	Serial.print(readSensor(MPU_ADDR, ACCEL_XOUT_L, ACCEL_XOUT_H));
+	Serial.print("\tA Y: ");
+	Serial.print(readSensor(MPU_ADDR, ACCEL_YOUT_L, ACCEL_YOUT_H));
+	Serial.print("\tA Z: ");
+	Serial.println(readSensor(MPU_ADDR, ACCEL_ZOUT_L, ACCEL_ZOUT_H));
+	Serial.print("G X: ");
+	Serial.print(readSensor(MPU_ADDR, GYRO_XOUT_L, GYRO_XOUT_H));
+	Serial.print("\tG Y: ");
+	Serial.print(readSensor(MPU_ADDR, GYRO_YOUT_L, GYRO_YOUT_H));
+	Serial.print("\tG Z: ");
+	Serial.println(readSensor(MPU_ADDR, GYRO_ZOUT_L, GYRO_ZOUT_H));
 	Serial.println();
+}
+
+int readSensor(byte DEV_ADDR, byte REG_L, byte REG_H){
+	return (int)((readByte(DEV_ADDR, REG_H) << 8) + readByte(DEV_ADDR, REG_L));
 }
